4c995d63ba0d5fa96f63a701a0a0db0b76eb4f63
efatha <163284022+efatha@users.noreply.github.com>
2024-11-06 23:15:56 +0200

    Added the menu sidebar features

--- script.js
+++ script.js
@@ -45,3 +45,19 @@ console.log(decreaseBtn, increaseBtn, countDisplay);
 `;
   text.style.display="flex";
  };
+
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("mySidenav").style.display = "flex";
+  document.getElementById("closebtn").style.display = "flex";
+  document.querySelector("main").style.opacity = "0.3";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.9)";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+  }
+    
